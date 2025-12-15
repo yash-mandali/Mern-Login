a45ed3fa44149e53fd3fc134d90a95c6b476b6d8
Yash Mandali <141387955+yash-mandali@users.noreply.github.com>
2025-12-15 12:14:31 +0530

    Change signup URL to production endpoint

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -26,7 +26,7 @@ function Signup() {
            handleError('All fields are required');
         }
         try {
-            const url = "http://localhost:8080/auth/signup";
+            const url = "https://mern-login-ten-chi.vercel.app/auth/signup";
             const response = await fetch(url, {
                 method: 'POST', 
                 headers: {
